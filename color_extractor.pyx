8c5062389058d70c668c70ca28312971473d884d
Ereli Eran <ereli.eran@gmail.com>
2015-04-24 18:07:02 +0100

    adding comments to the code

--- color_extractor.pyx
+++ color_extractor.pyx
@@ -1,68 +1,62 @@
 
 # coding: utf-8
 
-# In[ ]:
-
+# inspired by this so answer http://stackoverflow.com/a/3244061/1265980
 import struct
 import Image
 import scipy
 import scipy.misc
 import scipy.cluster
 import numpy as np
+
 def main_color(filename):
 
 	NUM_CLUSTERS = 8
 
 	#print 'reading image'
-	#filename = '/home/ereli/projects/play-images/ZZPdzvlpK9r_Df9C3M7j1rNRi7hhHRvPhlklJ3lfi5jk86Jd1s0Y5wcQ1QgbVaAP5Q=w300.png'
+	
 	im = Image.open(filename)
 
 
 	#im = im.resize((150, 150))	  # optional, to reduce time
-	im = im.convert("RGB")
+	im = im.convert("RGB") # it's necessary  to convert to RGBA from RGBA in order to remove the redundant alpha channel.
 	ar = scipy.misc.fromimage(im)
 	shape = ar.shape
 
 	ar = ar.reshape(scipy.product(shape[:2]), shape[2])
 
-	#print 'finding clusters'
+	# finding clusters
 	codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-	#print 'cluster centres:\n', codes
-
+	
 	vecs, dist = scipy.cluster.vq.vq(ar, codes)		 # assign codes
 	counts, bins = scipy.histogram(vecs, len(codes))	# count occurrences
-	#print counts
+	
 	index_max = scipy.argmax(counts)					# find most frequent
 
-	peak = codes[index_max]
+	#peak = codes[index_max] # this is the top color
 	peak_arr = []
 
 
-	colour = ''.join(chr(c) for c in peak).encode('hex')
+	#colour = ''.join(chr(c) for c in peak).encode('hex')
 	#print 'most frequent is %s (#%s)' % (peak, colour)
-	print len(counts)
+
+	# sometimes we only get 3 colors, so we check i order to not to print the top 4 if there are only 3.
+	# we then return the top 4 colors. 
+
 	if len(counts) >= 4:
 		ind = np.argpartition(counts, -4)[-4:]
 		ind = ind[np.argsort(counts[ind])]
 		ind = ind.tolist()
-	#print ind
+	
 		for i in range(0,len(ind)):
-		#print i
-		#peak_arr.append(ind[i])
-			peak_arr.append(''.join(chr(c) for c in codes[i]).encode('hex'))
+			peak_arr.append(''.join(chr(c) for c in codes[i]).encode('hex')) # appending the colors into a list
 	else:
 		length = len(counts)
 		ind = np.argpartition(counts, -length)[-length:]
 		ind = ind[np.argsort(counts[ind])]
 		ind = ind.tolist()
-	#print ind
+	
 		for i in range(0,len(ind)):
-		#print i
-		#peak_arr.append(ind[i])
 			peak_arr.append(''.join(chr(c) for c in codes[i]).encode('hex'))
-
-
-
-	#print peak_arr
-	return peak_arr
+	return peak_arr # returing the top 4 colors list
 
