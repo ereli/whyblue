4b989f60c60ca702aa8917dc2a5c4d28e001fcb4
Ereli Eran <ereli.eran@gmail.com>
2015-05-06 18:31:50 +0100

    adding redis based queue system

--- downloader.py
+++ downloader.py
@@ -3,38 +3,24 @@ import shutil
 from sys import argv
 import urllib2
 import urlparse
+import download
 input_filename = argv[1]
 folder = argv[2]
 print argv
 
-def download(url):
-	parsed_url = urlparse.urlparse(url)
-	#print url
-	filename = os.path.split(parsed_url.path)
-	try:
-		#
-		full_path = (folder+filename[1][0:3]+"/"+filename[1]).strip()
-		#print full_path
-		if os.path.exists(full_path) is False:
-			if os.path.exists(folder+filename[1][0:3]) is False:		
-				os.path.makedirs(folder+filename[1][0:3])
-		print("file doesn't exists, downloading...")
-		output = open(full_path,'wb')
-		file = urllib2.urlopen(url)
-		output.write(file.read())
-		output.close()
-		print("file written")
-		#print full_path
-	except:
-		raise
+
 
 		#if filename[1][""] os.path.isfile(path+i)
 	#file = urllib2.urlopen(url)
 	#print(parsed_url.path)
+from redis import Redis
+from rq import Queue
+
+q = Queue(connection=Redis())
 
 for line in open(input_filename):
-    download(line)
-
+	#download(line)
+	q.enqueue(download.download, line, folder)
 
 
 #output = open('test.mp3','wb')
